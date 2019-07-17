87a2101325c6b509ec0fc9ef0e7a069cba9072cf
Gergely Horvath <gergelyzsolt.hrv@gmail.com>
2019-07-17 15:52:00 +0200

    106. vid comes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Default imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 // Component imports
 import AppRouter from './routers/AppRouter';
@@ -17,12 +18,18 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
+
 const ExpenseDashboardPage = () => (
-	<div>asd</div>
+	<div>
+		<ExpenseListFilters />
+		<ExpenseList />
+	</div>
 );
 
 export default ExpenseDashboardPage;
